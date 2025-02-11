1d2c0808a428d363a28097e3b7a29d53634b49e7
9324868108 <166347451+9324868108@users.noreply.github.com>
2025-02-11 21:37:18 +0530

    Create helpers.js

---
+++ services/utils/helpers.js
@@ -0,0 +1,25 @@
+export function debounce(func, wait) {
+    let timeout;
+    return function executedFunction(...args) {
+        const later = () => {
+            clearTimeout(timeout);
+            func(...args);
+        };
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+    };
+}
+
+export function validateTrainNumber(trainNumber) {
+    return /^\d{5}$/.test(trainNumber);
+}
+
+export function formatDateTime(date) {
+    return new Intl.DateTimeFormat('en-IN', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit'
+    }).format(new Date(date));
+}
